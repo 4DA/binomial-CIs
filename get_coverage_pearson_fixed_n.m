20e2d4e82e884ce532cc562032ffa466e39a2439
Dmitry Cherkassov <dcherkassov@gmail.com>
2012-02-12 23:49:10 +0300

    optimized get_coverage_pearson_fixed_n

--- get_coverage_pearson_fixed_n.m
+++ get_coverage_pearson_fixed_n.m
@@ -3,23 +3,24 @@ function p = get_coverage_pearson_fixed_n (pc, nc)
 %% input:
 % pc  - probability 
 % nc  - number of trials
-    pc
+    pc 
+    persistent cis cus nnc;
+    
     alpha = 0.05;
-    p = 0;    
-    p1=zeros(nc+1,1);
-    p2=zeros(nc+1,1);
+    
+    if (isempty(nnc) | (nnc ~= nc))
+        nnc = nc;
+        cis=zeros(nc+1,1);
+        cus=zeros(nc+1,1);
 
-    for x=0:nc
-        [phat,pci] = binofit(x,nc,alpha);
-        p1(x+1)=pci(1); 
-        p2(x+1)=pci(2);
-    end;
-
-    for x=0:nc                         
-        if (pc >= p1(x+1)) & (pc <= p2(x+1))
-            p = p + binopdf(x, nc, pc);
+        for x=0:nc
+            [phat,pci] = binofit(x,nc,alpha);
+            cis(x+1)=pci(1); 
+            cus(x+1)=pci(2);
         end
+
     end
 
+    p = coverage_prob_fixed_n(cis, cus, pc, nc);
 end
 
