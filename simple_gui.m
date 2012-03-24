60fa0cf5e71817df949277f4709e2c0a14499f43
Dmitry Cherkassov <dcherkassov@gmail.com>
2012-03-24 20:38:11 +0300

    Added individual CI value manipulation

--- simple_gui.m
+++ simple_gui.m
@@ -22,7 +22,7 @@ function varargout = simple_gui(varargin)
 
 % Edit the above text to modify the response to help simple_gui
 
-% Last Modified by GUIDE v2.5 21-Mar-2012 01:02:40
+% Last Modified by GUIDE v2.5 24-Mar-2012 20:37:40
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -489,6 +489,148 @@ function shift_to_edit_Callback(hObject, eventdata, handles)
 % --- Executes during object creation, after setting all properties.
 function shift_to_edit_CreateFcn(hObject, eventdata, handles)
 % hObject    handle to shift_to_edit (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on slider movement.
+function ci_number_slider_Callback(hObject, eventdata, handles)
+% hObject    handle to ci_number_slider (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'Value') returns position of slider
+%        get(hObject,'Min') and get(hObject,'Max') to determine range of slider
+
+
+% --- Executes during object creation, after setting all properties.
+function ci_number_slider_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to ci_number_slider (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: slider controls usually have a light gray background.
+if isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor',[.9 .9 .9]);
+end
+
+
+
+function ci_number_edit_Callback(hObject, eventdata, handles)
+% hObject    handle to ci_number_edit (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of ci_number_edit as text
+%        str2double(get(hObject,'String')) returns contents of ci_number_edit as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function ci_number_edit_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to ci_number_edit (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on button press in setci_button.
+function setci_button_Callback(hObject, eventdata, handles)
+% hObject    handle to setci_button (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --- Executes on slider movement.
+function left_limit_slider_Callback(hObject, eventdata, handles)
+% hObject    handle to left_limit_slider (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'Value') returns position of slider
+%        get(hObject,'Min') and get(hObject,'Max') to determine range of slider
+
+
+% --- Executes during object creation, after setting all properties.
+function left_limit_slider_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to left_limit_slider (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: slider controls usually have a light gray background.
+if isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor',[.9 .9 .9]);
+end
+
+
+
+function left_limit_edit_Callback(hObject, eventdata, handles)
+% hObject    handle to left_limit_edit (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of left_limit_edit as text
+%        str2double(get(hObject,'String')) returns contents of left_limit_edit as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function left_limit_edit_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to left_limit_edit (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on slider movement.
+function slider14_Callback(hObject, eventdata, handles)
+% hObject    handle to slider14 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'Value') returns position of slider
+%        get(hObject,'Min') and get(hObject,'Max') to determine range of slider
+
+
+% --- Executes during object creation, after setting all properties.
+function slider14_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to slider14 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: slider controls usually have a light gray background.
+if isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor',[.9 .9 .9]);
+end
+
+
+
+function right_limit_edit_Callback(hObject, eventdata, handles)
+% hObject    handle to right_limit_edit (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of right_limit_edit as text
+%        str2double(get(hObject,'String')) returns contents of right_limit_edit as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function right_limit_edit_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to right_limit_edit (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    empty - handles not created until after all CreateFcns called
 
